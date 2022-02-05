d40af577496c30bf79775089124aac8158dc0cfb
Maciej Przybyła <95914501+SmolPrzybs@users.noreply.github.com>
2022-02-05 23:48:43 +0100

    Add files via upload

---
+++ Lab5z4/Lab5z4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+long silnia(int n)
+{
+    if (n < 2)
+        return 1; 
+    return n * silnia(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    cout << "Silnia z niej = " << silnia(n) << endl;
+    return 0;
+}
